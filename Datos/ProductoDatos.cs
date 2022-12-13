5945335141c7b64fe4db7546e0449c87f2c3c02c
emanuel oviedo <cjo1@live.com>
2022-12-12 22:13:15 -0300

    capa datos + getProductos

--- Datos/ProductoDatos.cs
+++ Datos/ProductoDatos.cs
@@ -1,7 +1,54 @@
 ﻿
+using Entidades;
+using System.Data.SqlClient;
+
 namespace Datos
 {
     public class ProductoDatos
     {
+        public static List<Producto> Get(Producto p)
+        {
+            List<Producto> list = new List<Producto>();
+
+            string conString = System.Configuration.ConfigurationManager.
+            ConnectionStrings["conexionDB"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("productosGet", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                if (p.id != null)
+                    command.Parameters.AddWithValue("@id", p.id);
+                if (p.nombre != null)
+                    command.Parameters.AddWithValue("@nombre", p.nombre);
+                if (p.precio != null)
+                    command.Parameters.AddWithValue("@apellido", p.precio);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Producto producto = new Producto();
+
+                        producto.id = Convert.ToInt32(reader["IDPROD"]);
+                        producto.nombre = Convert.ToString(reader["NOMBRE"]);
+
+
+                        list.Add(producto);
+
+                    }
+                    reader.Close();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+            return list;
+
+        }
     }
 }
--- Entidades/Estado.cs
+++ Entidades/Estado.cs
@@ -3,6 +3,6 @@
     public class Estado
     {
         public int id { get; set; }
-        public string nombre { get; set; }
+        public string? nombre { get; set; }
     }
 }
